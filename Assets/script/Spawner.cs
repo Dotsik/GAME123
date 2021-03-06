ec6016c4a4b9881c824e7e18b7386e2d563658a8
Alex Chernyak <sashachernyak15@gmail.com>
2021-03-06 19:10:52 +0200

    debugged mob spawn, + parent mob

--- Assets/script/Spawner.cs
+++ Assets/script/Spawner.cs
@@ -1,35 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Spawner : MonoBehaviour
 {
-    
+
     public GameObject Cube;
     public float timer;
     public Transform player;
     public float move_speed;
     public float rotation_speed;
     public Transform enemy;
-    public Vector3 vector3;
-    
-   
+    public Vector3 vector3, vector2;
+
+    Vector3 RandomBetweenRadius2D(float minRad,float y, float maxRad)
+    {
+        float radius = UnityEngine.Random.Range(minRad, maxRad);
+        float angle = UnityEngine.Random.Range(0, 360);
+
+        float x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+        float z = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+
+        return new Vector3(x, y, z);
+    }
+
     IEnumerator SpawnCD()
     {
-        //var cube = Instantiate<GameObject>(Cube);
 
-        vector3 = new Vector3(10, 0, 10);
+        vector3 = RandomBetweenRadius2D(60, player.position.y, 100);
+
         yield return new WaitForSeconds(timer);
         var look_dir = player.position - enemy.position;
         look_dir.y = 0;
         enemy.rotation = Quaternion.Slerp(enemy.rotation, Quaternion.LookRotation(look_dir), rotation_speed * Time.deltaTime);
         enemy.position += enemy.forward * move_speed * Time.deltaTime;
-        Instantiate(Cube, player.position+vector3, Quaternion.identity);
+        var children = Instantiate(Cube, vector3, Quaternion.identity) as GameObject;
+        children.GetComponent<Enemy>().enabled=true;
         Repeat();
+        
     }
 
     void Start()
     {
+
         StartCoroutine(SpawnCD());
     }
 
@@ -37,10 +51,10 @@ public class Spawner : MonoBehaviour
     {
         StartCoroutine(SpawnCD());
     }
-    
+
     void Update()
     {
-        
+
     }
 
 }
