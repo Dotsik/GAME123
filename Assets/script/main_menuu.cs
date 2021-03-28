e21a2273a48355b9856cddee96b039f7649c9598
Dotsik <70441934+Dotsik@users.noreply.github.com>
2021-03-28 11:14:01 +0300

    meinmenu

---
+++ Assets/script/main_menuu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class main_menuu : MonoBehaviour
+{
+    public Button start;
+    public Button Option;
+    public Button Continue;
+    public Button Exit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Continue.Active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
