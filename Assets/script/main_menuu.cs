34a5ab2aef6f25b096ac4e023a84f4d9d5a9b092
Dotsik <70441934+Dotsik@users.noreply.github.com>
2021-04-04 12:37:24 +0300

    current anim

--- Assets/script/main_menuu.cs
+++ Assets/script/main_menuu.cs
@@ -2,27 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
-using UnityEngine.SceneManagement;
 
 public class main_menuu : MonoBehaviour
 {
-    public void load_new_game()
-    {
-        SceneManager.LoadScene("main");
-    }
-    public void load_game()
-    {
+    public Button start;
+    public Button Option;
+    public Button Continue;
+    public Button Exit;
 
-    }
-    public void settings()
+    // Start is called before the first frame update
+    void Start()
     {
-        SceneManager.LoadScene("parametrs");
+       //Continue.Active = false;
     }
-    public void exit()
-    {
 
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
